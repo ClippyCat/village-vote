26810cc493e54ae01db18d6efb517587b3e247c4
Tait Hoyem <tait@tait.tech>
2023-09-23 19:12:17 -0600

    Use conditional in choosing input type: option.calendar creates a date input

--- src/components/OptionInput.js
+++ src/components/OptionInput.js
@@ -2,23 +2,43 @@
 import React, { useState } from "react";
 
 const OptionInput = ({ option, qIndex, oIndex, handleOptionChange, handleDateChange, handleStartTimeChange, handleEndTimeChange, handleTimezoneChange, removeOption }) => {
-  return (
-    <div>
-      <label>
-        Option {oIndex + 1} Date:
-        <input
-          type="date"
-          name={`date-${qIndex}-${oIndex}`}
-          value={option.date}
-          onChange={(e) => handleDateChange(qIndex, oIndex, e.target.value)}
-          required
-        />
-      </label>
-      {/* Include other option input fields (startTime, endTime, timezone) */}
-      {/* Add or remove options */}
-      <button onClick={() => removeOption(qIndex, oIndex)}>Remove Option</button>
-    </div>
-  );
+  if (option.calendar) {
+    return (
+      <div>
+        <label>
+          Option {oIndex + 1} Date:
+          <input
+            type="date"
+            name={`date-${qIndex}-${oIndex}`}
+            value={option.date}
+            onChange={(e) => handleDateChange(qIndex, oIndex, e.target.value)}
+            required
+          />
+        </label>
+        {/* Include other option input fields (startTime, endTime, timezone) */}
+        {/* Add or remove options */}
+        <button onClick={() => removeOption(qIndex, oIndex)}>Remove Option</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <label>
+          Option {oIndex + 1} Date:
+          <input
+            type="text"
+            name={`option-${qIndex}-${oIndex}`}
+            value={option.value}
+            onChange={(e) => handleOptionChange(qIndex, oIndex, e.target.value)}
+            required
+          />
+        </label>
+        {/* Include other option input fields (startTime, endTime, timezone) */}
+        {/* Add or remove options */}
+        <button onClick={() => removeOption(qIndex, oIndex)}>Remove Option</button>
+      </div>
+    );
+  }
 };
 
 export default OptionInput;
