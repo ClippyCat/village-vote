3c325cf260306d21579e3814e074f38cc078063f
melody580085 <melody580085@gmail.com>
2023-11-19 20:40:05 -0700

    vote rank

--- src/app/vote/Vote.js
+++ src/app/vote/Vote.js
@@ -14,6 +14,11 @@ const data = {
       options: ["b1", "b2"],
       type: "singleSelect",
     },
+    {
+      text: "q3",
+      options: ["r1", "r2", "r3", "r4"],
+      type: "rank",
+    },
   ]
 };
 
@@ -28,6 +33,8 @@ const Vote = () => {
             <SingleSelectOptions qIndex={qIndex} options={question.options} />
           ) : question.type === "multiSelect" ? (
             <MultiSelectOptions qIndex={qIndex} options={question.options} />
+          ) : question.type === "rank" ? (
+            <Rank qIndex={qIndex} options={question.options} />
           ) : null}
         </div>
       ))}
@@ -41,6 +48,7 @@ const SingleSelectOptions = ({ qIndex, options }) => {
       {options.map((option, oIndex) => (
         <label key={oIndex}>
           <input type="radio" name={`q${qIndex}`} value={option} />
+          {option}
         </label>
       ))}
     </div>
@@ -53,10 +61,52 @@ const MultiSelectOptions = ({ qIndex, options }) => {
       {options.map((option, index) => (
         <label key={index}>
           <input type="checkbox" name={`q${qIndex}`} value={option} />
+          {option}
         </label>
       ))}
     </div>
   );
+};
+
+const Rank = ({ qIndex, options }) => {
+  const [rankings, setRankings] = useState({});
+
+  const handleRankChange = (option, value) => {
+    const updatedRankings = { ...rankings, [option]: value };
+    setRankings(updatedRankings);
+  };
+
+  const handleSelectChange = (option, event) => {
+    const newValue = parseInt(event.target.value);
+    const currentRankings = { ...rankings };
+
+    if (!Object.values(currentRankings).includes(newValue)) {
+      handleRankChange(option, newValue);
+    } else {
+      handleRankChange(option, "");
+    }
+  };
+
+  const renderOptions = () =>
+    options.map((option, index) => (
+      <label key={option}>
+        {option}:
+        <select
+          name={`q${qIndex}-${index}`} // Unique name for each select
+          value={rankings[option] || ""}
+          onChange={(e) => handleSelectChange(option, e)}
+        >
+
+          {options.map((_, idx) => (
+            <option key={idx} value={idx}>
+              {idx + 1}
+            </option>
+          ))}
+        </select>
+      </label>
+    ));
+
+  return <div>{renderOptions()}</div>;
 };
 
 export default Vote;
