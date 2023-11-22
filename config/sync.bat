d2099f7544c56cb071c68cb82d96ce6a7e521f23
melody580085 <melody580085@gmail.com>
2023-11-22 16:28:56 -0700

    try again

--- config/sync.bat
+++ config/sync.bat
@@ -1,3 +1,3 @@
 cd C:\Users\Melody\Desktop\bs\programming\myrepo\village-vote
 git push -u origin main
-ssh arch@clippycat.ca "cd village-vote; git pull origin main --rebase; sudo cp config/village-vote.service /etc/systemd/system/; sudo systemctl daemon-reload; sudo systemctl restart village-vote; python /src/db/connection.py"
+ssh arch@clippycat.ca "cd village-vote; git pull origin main --rebase; sudo cp config/village-vote.service /etc/systemd/system/; sudo systemctl daemon-reload; sudo systemctl restart village-vote; python src/db/connection.py"
